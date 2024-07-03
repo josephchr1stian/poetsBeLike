41160532980d26ca07d375c04ff77be547303276
Christian <josephcgarcia1001@gmail.com>
2024-07-03 15:18:24 -0700

    I got matching to work :)

--- src/App.js
+++ src/App.js
@@ -10,57 +10,79 @@ import Grid from "@mui/material/Grid";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Tooltip from "@mui/material/Tooltip";
-import IconButton from "@mui/material/IconButton";
-
 import "./App.css";
-import characters from './protagonists.json';
-import Icon from '@mui/material/Icon';
-import OutletIcon from '@mui/icons-material/Outlet';
-import CharacterCard from './CharacterCard'
-
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-
-
-
-import { ClickAwayListener } from '@mui/base/ClickAwayListener';
-import { useState } from 'react';
-
-import fetchPoem from './CharacterCard'
-
-
-
-
+import characters from "./protagonists.json";
+import CharacterCard from "./CharacterCard";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { useState } from "react";
+import fetchPoem from "./CharacterCard";
+import { Stack } from "@mui/material";
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
 
 function App() {
+  const [status, setStatus] = useState("");
+  const [authorList, setAuthorList] = useState([]);
+  const [authorChoice, setAuthorChoice] = useState("Emily Dickinson");
+  const [authorInput, setAuthorInput] = useState(authorChoice);
+  const [wordChoices, setWordChoices] = useState([]);
+  const [wordOne, setWordOne] = useState("");
+  const [wordTwo, setWordTwo] = useState("");
+  const [wordThree, setWordThree] = useState("");
+  const [userScore, setUserScore] = useState(0);
+  const [poemList, setPoemList] = useState();
+  const [matchPoems, setMatch] = useState();
 
-  console.log("Chars from JSON", characters);
-  const [status, setStatus] = useState('')
-  const[authorList, setAuthorList] = useState([]);
-  const [authorChoice, setAuthorChoice] = useState("Dickinson")
- 
- 
- 
   function fetchAuthorList() {
-
-
     const requestOptions = {
       method: "GET",
-      redirect: "follow"
-    };  
+      redirect: "follow",
+    };
     fetch("https://poetrydb.org/author", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      
-      console.log(result.authors)
-      console.log(authorList)
-      setAuthorList(result.authors)
-    })
-    .catch((error) => console.error(error));
-  
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result.authors);
+        console.log(authorList);
+        setAuthorList(result.authors);
+      })
+      .catch((error) => console.error(error));
   }
-  
+  function showMatches(poetName) {
+    console.log("From Show Matches", poemList);
+    const matches = new Set(); // will hold the matched entry
+    poemList.map((entry) => {
+      let allWordString = entry.lines.join(" ").toLowerCase(); //join the array of lines and make it all lower case
+      let wordArray = allWordString.split(" "); // Split the string into an array of words
+      const wordSet = new Set(wordArray); // make a set of all words
+      const answerSet = new Set(wordChoices);
+      for (let i of answerSet) {
+        if (wordSet.has(i)) {
+          matches.add(entry)
+        }
+      }
+    });
+    console.log(matches);
+    setMatch(matches);
+  }
+  function fetchPoems(poetName) {
+    //let poemNum = Math.floor(Math.random() * 10);
+    console.log("you are trying to fetch", poetName);
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+    fetch("https://poetrydb.org/author/" + poetName, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        let res = result;
+        //console.log("From Poem Function Call", res);
+        //put the object in a var
+        setPoemList(res);
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
     <div className="App">
       <CssBaseline />
@@ -85,7 +107,6 @@ function App() {
         </Toolbar>
       </AppBar>
       <Container maxWidth="md" sx={{ my: 4 }}>
-      
         <Typography
           variant="h2"
           align="center"
@@ -94,24 +115,93 @@ function App() {
         >
           poetsBeLike.
         </Typography>
-        <Autocomplete
-      
-          disablePortal
-          
-          id="combo-box-demo"
-          options={authorList}
-          sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Poets" />}                    
-        />
-    
+        <Stack direction="row" spacing={15}>
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={authorList}
+            onChange={(event, newValue) => {
+              setAuthorInput(newValue);
+              console.log("New Value is " + authorInput);
+            }}
+            inputValue={authorInput}
+            onInputChange={(event, newInputValue) => {
+              setAuthorChoice(newInputValue);
+              setAuthorInput(newInputValue);
+            }}
+            value={authorChoice}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Poets" />}
+          />
+          <Button
+            sx={{
+              color: "black",
+              px: 3,
+              mx: "auto",
+              border: "1px solid black",
+            }}
+            onClick={() => {
+              // Send the words to chars and display the matching poems
+
+              setWordChoices([wordOne, wordTwo, wordThree]);
+              console.log("Word Choices are: " + wordChoices);
+              //console.log(authorChoice)
+              console.log(authorInput);
+              showMatches(authorChoice);
+            }}
+          >
+            lock In
+          </Button>
+          <Typography variant="h3">scoree: {userScore}</Typography>
+        </Stack>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1 },
+            p: 3,
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            defaultValue=""
+            value={wordOne}
+            onChange={(event) => {
+              //console.log(newInputValue);
+              let newInputValue = event.target.value;
+              setWordOne(newInputValue);
+              //console.log(wordOne);
+            }}
+          />
+          <TextField
+            defaultValue=""
+            value={wordTwo}
+            onChange={(event) => {
+              //console.log(newInputValue);
+              let newInputValue = event.target.value;
+              setWordTwo(newInputValue);
+              //console.log(wordTwo);
+            }}
+          />
+          <TextField
+            defaultValue=""
+            value={wordThree}
+            onChange={(event) => {
+              //console.log(newInputValue);
+              let newInputValue = event.target.value;
+              setWordThree(newInputValue);
+              //console.log(wordThree);
+            }}
+          />
+        </Box>
         <Typography
           variant="h5"
           align="center"
           color="text.secondary"
           sx={{ mx: 10 }}
-          id = "subtitle"
+          id="subtitle"
         >
-         {status}
+          {status}
         </Typography>
       </Container>
       {/* End hero unit */}
@@ -123,37 +213,19 @@ function App() {
           justifyContent="center"
           alignItems="flex-start"
         >
-
           {characters.map((entry) => (
-          <Grid item xs={12} md={4}>
-            <CharacterCard title = {entry.title}
-              image = {entry.pic} description = {entry.description} 
-            /> 
-          </Grid>
+            <Grid item xs={12} md={4}>
+              <CharacterCard
+                title={entry.title}
+                image={entry.pic}
+                description={entry.description}
+              />
+            </Grid>
           ))}
-        </Grid> 
-
+        </Grid>
       </Container>
     </div>
   );
 }
 
-function fetchFact() {
-
-
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow"
-  };  
-  fetch("https://uselessfacts.jsph.pl/api/v2/facts/random", requestOptions)
-  .then((response) => response.json())
-  .then((result) => {
-    console.log(result.text)
-    return(result.text)
-    //let subtitle  = document.getElementById("subtitle")
-    //subtitle.innerHTML = myFact;
-  })
-  .catch((error) => console.error(error));
-
-}
 export default App;
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,142 +1,117 @@
-
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import CardHeader from "@mui/material/CardHeader";
-import { useState } from 'react';
-
+import { useState } from "react";
 
 export default function CharacterCard(props) {
-  const [counter, setCounter] = useState(0)
-  const [poemLines, setPoemLines] = useState([])
-  const[poemImage, setPoemImage] = useState("https://picsum.photos/300?grayscale&blur=3")
-  const [poemTitle, setPoemTitle] = useState("")
-  const[commonWord, setCommonWord ] = useState("helios")
+  const [counter, setCounter] = useState(0);
+  const [poemLines, setPoemLines] = useState([]);
+  const [poemImage, setPoemImage] = useState(
+    "https://picsum.photos/300?grayscale&blur=3"
+  );
+  const [poemTitle, setPoemTitle] = useState("");
+  const [commonWord, setCommonWord] = useState("helios");
 
   return (
     <Card>
-              <CardMedia
-                component="img"
-                height="350px"
-                image = {poemImage}
-              />
-              <CardHeader
-                //title={props.title}
-                title = {poemTitle}
-                titleTypographyProps={{ align: "center" }}
-                sx={{ mt: 1 }}
-              />
-              <CardContent sx={{ pt: 0 }}>
-                <ul>
-          
-                {poemLines.map((line, index) => (
-
-                <Typography >
-                  {index+1} - {line}
-                </Typography>
-                  ))}     
-
-                  <Typography variant = "h1" > {counter}</Typography> 
-                  <Typography variant = "h3" > bands</Typography>
-                               
-                </ul>
-              </CardContent>
-              <CardActions>
-                <Button
-                  //variant="contained"
-                  sx={{ px: 6, mx: "auto", border: "1px solid black", variant: "outlined" }}   
-                  onClick={() => {
-                    fetchPoem();
-                    findMostCommonWord(poemLines.join(' '))
-                    setImage();
-                    setCounter(counter + 1);
-                  }}  
-                        
-                >
-                 click 2 give a dollar  
-                
-                </Button>
-                
-              </CardActions>
-    </Card>  
-  )
+      <CardMedia component="img" height="350px" image={poemImage} />
+      <CardHeader
+        //title={props.title}
+        title={poemTitle}
+        titleTypographyProps={{ align: "center" }}
+        sx={{ mt: 1 }}
+      />
+      <CardContent sx={{ pt: 0 }}>
+        <ul>
+          {poemLines.map((line, index) => (
+            <Typography>
+              {index + 1} - {line}
+            </Typography>
+          ))}
+        </ul>
+      </CardContent>
+      <CardActions>
+        <Button
+          //variant="contained"
+          sx={{
+            px: 6,
+            mx: "auto",
+            border: "1px solid black",
+            variant: "outlined",
+          }}
+          onClick={() => {
+            fetchPoem();
+            findMostCommonWord(poemLines.join(" "));
+            setImage();
+            setCounter(counter + 1);
+          }}
+        >
+          load poem
+        </Button>
+      </CardActions>
+    </Card>
+  );
 
   function fetchPoem() {
     let poemNum = Math.floor(Math.random() * 10);
     const requestOptions = {
       method: "GET",
-      redirect: "follow"
-    };  
+      redirect: "follow",
+    };
     fetch("https://poetrydb.org/author/Dickinson", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      let res = result[poemNum]
-      console.log(res)
-      setPoemLines(result[poemNum].lines)
-      setPoemTitle(res.title)
-    })
-    .catch((error) => console.error(error));
-  
+      .then((response) => response.json())
+      .then((result) => {
+        let res = result[poemNum];
+        console.log(res);
+        setPoemLines(result[poemNum].lines);
+        setPoemTitle(res.title);
+      })
+      .catch((error) => console.error(error));
   }
 
-
-  function setImage(){
+  function setImage() {
     let blurLevel = Math.floor(Math.random() * 3);
-    console.log(blurLevel)
-    console.log("https://picsum.photos/300?grayscale&blur="+ toString(blurLevel))
-    setPoemImage("https://picsum.photos/300?grayscale&blur="+ toString(blurLevel))
-
+    console.log(blurLevel);
+    console.log(
+      "https://picsum.photos/300?grayscale&blur=" + toString(blurLevel)
+    );
+    setPoemImage(
+      "https://picsum.photos/300?grayscale&blur=" + toString(blurLevel)
+    );
   }
 
   function findMostCommonWord(str) {
     // Remove punctuation and convert string to lowercase
-    const cleanedStr = str.replace(/[^\w\s]/g, '').toLowerCase();
+    const cleanedStr = str.replace(/[^\w\s]/g, "").toLowerCase();
 
     // Split the string into words
     const words = cleanedStr.split(/\s+/);
 
     // Create a frequency map
     const wordMap = {};
-    words.forEach(word => {
-        if (wordMap[word]) {
-            wordMap[word]++;
-        } else {
-            wordMap[word] = 1;
-        }
+    words.forEach((word) => {
+      if (wordMap[word]) {
+        wordMap[word]++;
+      } else {
+        wordMap[word] = 1;
+      }
     });
 
     // Find the most common word
     let maxCount = 0;
-    let mostCommonWord = '';
+    let mostCommonWord = "";
     for (const word in wordMap) {
-        if (wordMap[word] > maxCount) {
-            maxCount = wordMap[word];
-            mostCommonWord = word;
-        }
+      if (wordMap[word] > maxCount) {
+        maxCount = wordMap[word];
+        mostCommonWord = word;
+      }
     }
-    console.log(mostCommonWord + " is the most common word")
+    console.log(mostCommonWord + " is the most common word");
     setCommonWord(mostCommonWord);
-}
-
-}
-
-
-
-function fetchFact() {
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow"
-  };  
-  fetch("https://uselessfacts.jsph.pl/api/v2/facts/random", requestOptions)
-  .then((response) => response.json())
-  .then((result) => {
-    console.log(result)
-    alert(result.text)
-  })
-  .catch((error) => console.error(error));
-
+  }
 }
