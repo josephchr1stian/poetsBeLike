07e93fd458248a53d973f14cef9d91d144fc4f79
Christian <josephcgarcia1001@gmail.com>
2024-07-05 09:27:20 -0700

    Checking other commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import "./App.css"; 
+import "./App.css";
 import CharacterCard from "./CharacterCard";
 import Card from "@mui/material/Card";
 //import CardMedia from "@mui/material/CardMedia";
@@ -13,10 +13,22 @@ import Autocomplete from "@mui/material/Autocomplete";
 import { useEffect, useState } from "react";
 import { Stack } from "@mui/material";
 import Box from "@mui/material/Box";
-import cutiveMono from "./CutiveMono-Regular.ttf"
+import cutiveMono from "./CutiveMono-Regular.ttf";
 import { RequestPageSharp } from "@mui/icons-material";
 
+export default function App() {
+  const [status, setStatus] = useState("");
+  const [authorList, setAuthorList] = useState([]);
+  const [authorChoice, setAuthorChoice] = useState("Emily Dickinson");
+  const [authorInput, setAuthorInput] = useState(authorChoice);
 
+  const [wordOne, setWordOne] = useState("");
+  const [wordTwo, setWordTwo] = useState("");
+  const [wordThree, setWordThree] = useState("");
+  const [userScore, setUserScore] = useState(0);
+  const [poemList, setPoemList] = useState();
+  const [matchPoems, setMatch] = useState([]);
+  const [titleWord, setTitleWord] = useState([]);
   function fetchAuthorList() {
     const requestOptions = {
       method: "GET",
@@ -34,21 +46,22 @@ import { RequestPageSharp } from "@mui/icons-material";
   function findMatches(poetName) {
     fetchPoems(poetName);
     const matches = new Set(); // will hold the matched entry
-    if(poemList){
-    poemList.map((entry) => {
-      let allWordString = entry.lines.join(" ").toLowerCase(); //join the array of lines and make it all lower case
-      let wordArray = allWordString.split(" "); // Split the string into an array of words
-      const wordSet = new Set(wordArray); // make a set of all words
-      const answerSet = new Set(wordChoices);
-      for (let i of answerSet) {
-        if (wordSet.has(i)) {
-          matches.add(entry);
+    if (poemList) {
+      poemList.map((entry) => {
+        let allWordString = entry.lines.join(" ").toLowerCase(); //join the array of lines and make it all lower case
+        let wordArray = allWordString.split(" "); // Split the string into an array of words
+        const wordSet = new Set(wordArray); // make a set of all words
+        const answerSet = new Set(wordChoices);
+        for (let i of answerSet) {
+          if (wordSet.has(i)) {
+            matches.add(entry);
+          }
         }
-      }
-    })};
+      });
+    }
     setMatch(Array.from(matches));
-    setUserScore(matches.size)
-    console.log("Log Matched objects", matches)
+    setUserScore(matches.size);
+    console.log("Log Matched objects", matches);
   }
   function fetchPoems(poetName) {
     //let poemNum = Math.floor(Math.random() * 10);
@@ -69,10 +82,10 @@ import { RequestPageSharp } from "@mui/icons-material";
     // Everything in here gets run once on first page load
     fetchAuthorList();
   }, []);
-  
+
   return (
     <div className="App">
-      <CssBaseline  />
+      <CssBaseline />
       <AppBar
         position="static"
         color="default"
@@ -83,7 +96,6 @@ import { RequestPageSharp } from "@mui/icons-material";
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             welcome.
           </Typography>
-
         </Toolbar>
       </AppBar>
       <Container maxWidth="md" sx={{ my: 4 }}>
@@ -96,15 +108,16 @@ import { RequestPageSharp } from "@mui/icons-material";
           poetsBeLike.
         </Typography>
         <Card
-            sx = {{
-              boxShadow: 0, 
-              py : 4}}>
+          sx={{
+            boxShadow: 0,
+            py: 4,
+          }}
+        >
           <CardMedia
             component="img"
             height="360"
             alt="randimg"
             image="https://picsum.photos/300?grayscale&blur=2"
-            
           />
         </Card>
         <Stack direction="row" spacing={15}>
@@ -113,33 +126,31 @@ import { RequestPageSharp } from "@mui/icons-material";
             id="combo-box-demo"
             options={authorList}
             onChange={(event, newValue) => {
-              if(newValue != null){
-              setAuthorInput(newValue);
-              console.log("New Value is " + authorInput);
-            }
+              if (newValue != null) {
+                setAuthorInput(newValue);
+                console.log("New Value is " + authorInput);
+              }
             }}
             inputValue={authorInput}
             onInputChange={(event, newInputValue) => {
-              if(newInputValue != null){
-              setAuthorChoice(newInputValue);
-              setAuthorInput(newInputValue);
-            }
+              if (newInputValue != null) {
+                setAuthorChoice(newInputValue);
+                setAuthorInput(newInputValue);
+              }
             }}
             value={authorChoice}
             sx={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="Poets" />}
           />
-          <Button 
-            size ="small"
+          <Button
+            size="small"
             sx={{
               color: "black",
               px: 2,
               border: "1px solid black",
             }}
             onClick={() => {
-
-
-              console.log(wordOne)
+              console.log(wordOne);
               //setTitleWord(wordOne)
               console.log("Titles containing: " + titleWord);
               setMatch(fetchTitleMatches(wordOne));
@@ -148,19 +159,19 @@ import { RequestPageSharp } from "@mui/icons-material";
           >
             search all titles
           </Button>
-          <Button 
-            size ="small"
+          <Button
+            size="small"
             sx={{
               color: "black",
               px: 2,
               border: "1px solid black",
             }}
             onClick={() => {
-              console.log(wordOne)
-              console.log(wordTwo)
-              console.log(wordThree)
+              console.log(wordOne);
+              console.log(wordTwo);
+              console.log(wordThree);
               console.log(authorInput);
-             setMatch(findMatches(authorChoice, wordOne, wordTwo, wordThree)); // Pass all the words to the find matches rather than making a new object/ set. set poens to what we find
+              setMatch(findMatches(authorChoice, wordOne, wordTwo, wordThree)); // Pass all the words to the find matches rather than making a new object/ set. set poens to what we find
             }}
           >
             search lines
@@ -227,17 +238,16 @@ import { RequestPageSharp } from "@mui/icons-material";
           alignItems="flex-start"
         >
           {[...matchPoems].map((entry, index) => (
-            <Grid item xs={12} md={4} key = {index}>
-            <CharacterCard
-              title={entry.title}
-              description={entry.lines}
-              keywords = {wordChoices}
-            />
-          </Grid>
+            <Grid item xs={12} md={4} key={index}>
+              <CharacterCard
+                title={entry.title}
+                description={entry.lines}
+                keywords={wordChoices}
+              />
+            </Grid>
           ))}
         </Grid>
       </Container>
     </div>
   );
 }
-export default App;
